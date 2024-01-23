221fa9bb15a415f39c0dc2440f3299781b6d61c1
Ramprasad57266 <157345293+Ramprasad57266@users.noreply.github.com>
2024-01-23 15:01:00 +0530

    Update streamlit_app.py
    added jackfruit code

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,10 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
+#New section to display fruitvice api response
+#streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('The user entered ', fruit_choice)
+streamlit.text("Thanks for adding jackfruit")
+
 
