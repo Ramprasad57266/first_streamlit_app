7b893071ad3137572b3748930a1853eecc934a15
Ramprasad57266 <157345293+Ramprasad57266@users.noreply.github.com>
2024-01-23 18:47:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake
 import snowflake.connector
 from urllib.error import URLError
 
@@ -10,8 +11,8 @@ streamlit.text('🥣Omega 3 and Oatmeal')
 streamlit.text('🥗Kale,Spinach & Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 #streamlit.dataframe(my_fruit_list)
@@ -20,29 +21,37 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+#New section to display fruitvice api response
+streamlit.header("Fruityvice Fruit Advice!")
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = request.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
   return fruityvice_normalized
-  
-#New section to display fruitvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
+  try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      back_from_function = get_fruityvice_data(fruit_choice)
+      #back_from_function = get_fruityvice_data(fruit_choice)
+    back_from_function = request.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
       streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-  
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#streamlit.text(fruityvice_response.json())
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
 streamlit.header("View Our Fruit List - Add Your Favorites!")
 #snowflake-related functions
 def get_fruit_load_list():
@@ -66,7 +75,8 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button("Add a fruit to the list"):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
-  streamlit.text(back_from_function)  
+  streamlit.text(back_from_function)
+  my_cnx.close()
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
